65140747971ddfb2c08a742022fa821e43ebc842
rahulsharma180 <rahulsharma18072022@gmail.com>
2024-12-30 17:56:07 +0530

    first commit

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Cart.module.css';
+
+function Cart() {
+  return (
+    <div className={styles.back}>
+      This is Cart
+    </div>
+  )
+}
+
+export default Cart
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Home.module.css';
+import Hero from '../../components/Hero/Hero';
+
+function Home() {
+  return (
+    <div className={styles.back}>
+
+      <Hero/>
+      
+    </div>
+  )
+}
+
+export default Home

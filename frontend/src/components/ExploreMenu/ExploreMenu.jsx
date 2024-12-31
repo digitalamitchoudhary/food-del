4e3798b6fba22012f332f9c36edf23f72cec0768
rahulsharma180 <rahulsharma18072022@gmail.com>
2024-12-31 17:52:36 +0530

    d

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './ExploreMenu.module.css'
+import { menu_list } from '../../assets/assets'
+
 
 function ExploreMenu() {
   return (
-    <div>
-      
+    <div className={styles.explore_menu} id={styles.explore_menu}>
+      <h1>Explore our menu </h1>
+      <p className={styles.explore_menu_text}>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+
+      <div className={styles.explore_menu_list}>
+        {menu_list.map((item,i)=>{
+
+        return(
+          
+          <div key={i} className={styles.explore_menu_list_item }> 
+          <img src={item.menu_image} alt="" />
+          <p>{item.menu_name}</p>
+          
+          </div>
+        )
+      })}</div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './Home.module.css';
 import Hero from '../../components/Hero/Hero';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 function Home() {
   return (
     <div className={styles.back}>
 
       <Hero/>
-      
+      <ExploreMenu/>
     </div>
   )
 }

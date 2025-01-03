1e9fab973316563160056497aa227f9344522c84
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-01-03 18:02:54 +0530

    ddD

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ExploreMenu.module.css'
+
+function FoodDisplay() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FoodDisplay

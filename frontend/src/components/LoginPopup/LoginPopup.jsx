232380cc92f1dd8e813da6c79459c4dc846925fb
rahulsharma180 <121606957+rahulsharma180@users.noreply.github.com>
2025-01-10 11:37:53 +0000

    dad

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './LoginPopup.module.css'
 function LoginPopup() {
+ 
+
+  const [currState,setCurrState]=useState("Sign-Up")
+
   return (
-    <div className={styles.loginPopup}>
-    <form  className={loginPopupContainer}>
+    <div className={styles.loginPopup}> 
+    <form  className={styles.loginPopupContainer}> 
+      <div className={styles.loginPopupTitle}>
+      <h2>{currState}</h2>
+      <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt=''></img>
+      </div>
+
+      
 
 
 

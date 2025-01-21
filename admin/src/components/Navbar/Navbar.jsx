301f4312c52e9c738db6e747038fff13a3b05c8f
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-01-21 18:03:13 +0530

    ss

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+     <div className={styles.ss} st>
+          fff
+        </div>
+  )
+}
+
+export default Navbar
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Sidebar.module.css'
+function Sidebar() {
+  return (
+    <div className={styles.ss} st>
+      fff
+    </div>
+  )
+}
+
+export default Sidebar

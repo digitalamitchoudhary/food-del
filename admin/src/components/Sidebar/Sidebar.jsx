0eb5c0fc419b675c9198d50679b2f3a55b211e3f
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2025-02-12 08:06:22 +0530

    dd

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -3,22 +3,37 @@ import styles from './Sidebar.module.css'
 import {assets} from '../../assets/assets'
 import {   NavLink } from 'react-router-dom';
 function Sidebar() {
+
+  const menuItems = [
+    { path: "add", icon: assets.add_icon, label: "Add Item" },
+    { path: "list", icon: assets.order_icon, label: "List Item" },
+    { path: "orders", icon: assets.order_icon, label: "Order" }
+  ];
+  
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarOptions}>
-        <NavLink to="add" className={styles.sidebarOption}>
+        {/* <NavLink to="add" className={styles.sidebarOption}>
           <img src={assets.add_icon} alt="" />
           <p>Add Item</p>
         </NavLink>
 
-        <NavLink  to="list"className={styles.sidebarOption}>
+        <NavLink   to="list"className={styles.sidebarOption}>
           <img src={assets.order_icon} alt="" />
           <p>List Item</p>
         </NavLink>
         <NavLink  to="orders" className={styles.sidebarOption}>
           <img src={assets.order_icon} alt="" />
           <p>Order</p>
-        </NavLink>
+        </NavLink> */}
+
+{menuItems.map(({ path, icon, label }) => (
+  <NavLink key={path} to={path} className={({ isActive }) => isActive ? `${styles.sidebarOption} ${styles.active}` : styles.sidebarOption}>
+    <img src={icon} alt={label} />
+    <p>{label}</p>
+  </NavLink>
+))}
+
       </div>
     </div>
   );
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import styles from './Add.module.css'
+import { assets } from '../../assets/assets'
 
 function Add() {
   return (
-    <div>
-      s
+    <div className={styles.add}>
+      <form className={styles.flexCol}>
+      <div className={`${styles.addImgUpload} ${styles.flexCol}`}>
+
+        <p>Upload Image</p>
+        <label htmlFor="image">
+
+            <img src={assets.upload_area} alt="" />
+        </label>
+        <input type="file" hidden required id="image" />
+      </div>
+
+      <div className={`${styles.addProductName} ${styles.flexCol}`}>
+
+        <p>Product name</p>
+        <input type="text" name="name" placeholder='Type here' />
+      </div>
+
+      <div className={`${styles.addProductDescription} ${styles.flexCol}`}>
+        <p>Product Description</p>
+        <textarea name="description"  rows="6" placeholder='Write content Here' required></textarea>
+      </div>
+      <div className={styles.addCategoryPrice}></div>
+  
+
+
+      </form>
     </div>
   )
 }

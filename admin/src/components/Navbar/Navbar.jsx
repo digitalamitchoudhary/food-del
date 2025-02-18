9b8644979e5594427519bd5a3305bd1ae50c8b10
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2025-02-19 01:07:23 +0530

    ff

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../../assets/assets'
 
 function Navbar() {
   return (
-     <div className={styles.navbar} st>
+     <div className={styles.navbar} >
           <img  className={styles.logo} src={assets.logo} alt="" />
           <img  className={styles.profile} src={assets.profile_image} alt="" />
         </div>

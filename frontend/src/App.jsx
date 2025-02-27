01821db366ccdd4d7ada229d07cf2ab12f06aef2
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-02-27 17:22:36 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
 import { useState } from 'react'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Verify from './pages/Verify/Verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -23,6 +25,9 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<PlaceOrder/>} />    
+        <Route path='/verify' element={<Verify/>} />    
+        <Route path='/myorders' element={<MyOrders/>} />    
+
 
       </Routes>
 

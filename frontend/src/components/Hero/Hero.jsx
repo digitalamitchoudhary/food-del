010f0700e79d22319a1ecf2fa511e39cbdd8ae42
rahulsharma180 <121606957+rahulsharma180@users.noreply.github.com>
2025-02-28 20:38:52 +0530

    Update Hero.jsx

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
 <div className={styles.hero} id="tab" >
 
 <div className={styles.header_contents}>
-  <h2>Order your favourite food here
+  <h2>Order your favourite food
   </h2>
   <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
 
